f6b2266f0272b4d607a29859c4b663761532c7e7
panossot <psotirop@redhat.com>
2020-01-26 16:17:20 +0100

    Test commit message ...

--- goat-server.ts
+++ goat-server.ts
@@ -46,16 +46,21 @@ app.route('/gitcommitpush/:gitnum/:gitrepo/:gitbranch/:clonedir/:gitcommitmessag
             }
         }
         const execSync = require('child_process').execSync;
-        const output = execSync('cd '+clonedir, { encoding: 'utf-8' });
-        console.log('cd'+clonedir+':\n', output);
-        const output0 = execSync('git add .', { encoding: 'utf-8' });
-        console.log('Git addition done:\n', output0);
-        const output1 = execSync('git commit -m "'+gitcommitmessage+'"', { encoding: 'utf-8' });
-        console.log('Git commit done:\n', output1);
-        const output2 = execSync('git push '+gitrepo+' '+gitbranch, { encoding: 'utf-8' });
-        console.log('Git push done:\n', output2);
-        res.status(200).send();
-        console.log('Commit and push of ', goaturl,' is done.');
+        if(clonedir!='') {
+            const output = execSync('cd '+clonedir, { encoding: 'utf-8' });
+            console.log('cd '+clonedir+':\n', output);
+            const output0 = execSync('git add .', { encoding: 'utf-8' });
+            console.log('Git addition done:\n', output0);
+            try {
+                const output1 = execSync('git commit -m "'+gitcommitmessage+'"', { encoding: 'utf-8' });
+                console.log('Git commit done:\n', output1);
+            }finally{
+                const output2 = execSync('git push '+gitrepo+' '+gitbranch, { encoding: 'utf-8' });
+                console.log('Git push done:\n', output2);
+                res.status(200).send();
+                console.log('Commit and push of ', goaturl,' is done.');
+            }
+        }
     });
     
 function redirectRouterLessonUnmatched(req,res) {
